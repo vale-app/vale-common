9322bbbdc9fdf4f7a24be5ad99165fab2c693de6
Tomas Flores Alvear <tom@Tomass-MacBook-Pro-2.local>
2022-01-27 16:28:34 -0300

    refactor: changes ProductId data type

--- models/store_product.go
+++ models/store_product.go
@@ -6,7 +6,7 @@ type StoreProduct struct {
 	gorm.Model
 	StoreId        uint    `json:"store_id"`
 	Category       string  `json:"category"`
-	ProductId      uint    `json:"product_id"`
+	ProductId      string  `json:"product_id"`
 	AvailableUnits int     `json:"available_units"`
 	ReservedUnits  int     `json:"reserved_units"`
 	Price          float32 `json:"price"`
