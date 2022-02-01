362a0f3bd6a54ff889d6c5b6593113f8e95c1d17
Tomas Flores Alvear <tom@Tomass-MacBook-Pro-2.local>
2022-02-01 15:40:19 -0300

    feat: adds enums for payment methodsgp

---
+++ enums/payment_methods.go
@@ -0,0 +1,9 @@
+package enums
+
+const (
+	PaymentMethodInStoreCash       = "IN_STORE_CASH"
+	PaymentMethodInStoreCreditCard = "IN_STORE_CREDIT_CARD"
+	PaymentMethodInStoreDebitCard  = "IN_STORE_DEBIT_CARD"
+	PaymentMethodValeCreditCard    = "VALE_CREDIT_CARD"
+	PaymentMethodValeDebitCard     = "VALE_DEBIT_CARD"
+)
