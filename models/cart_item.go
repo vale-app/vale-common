ed2dadf9408cef77f876b050d97dce7b3bfa1366
Tomas Flores Alvear <tom@Tomass-MacBook-Pro-2.local>
2022-02-10 22:26:15 -0300

    feat: adds product id to cart item

--- models/cart_item.go
+++ models/cart_item.go
@@ -8,7 +8,7 @@ type CartItem struct {
 	gorm.Model
 	CartId         int     `json:"cart_id" bson:"cart_id"`
 	StoreProductId int     `json:"store_product_id"`
+	ProductId      string  `json:"product_id"`
 	Quantity       int     `json:"quantity"`
 	Price          float64 `json:"price"`
-	Product        Product `json:"product"`
 }
