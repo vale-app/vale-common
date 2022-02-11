0419e15d2e27f18d9b6a4be8bffffa8c66f1ae61
Tomas Flores Alvear <tom@Tomass-MacBook-Pro-2.local>
2022-02-10 23:27:49 -0300

    fix: changes Product to reference type

--- models/cart_item.go
+++ models/cart_item.go
@@ -6,10 +6,10 @@ import (
 
 type CartItem struct {
 	gorm.Model
-	CartId         int     `json:"cart_id" bson:"cart_id"`
-	StoreProductId int     `json:"store_product_id"`
-	ProductId      string  `json:"product_id"`
-	Quantity       int     `json:"quantity"`
-	Price          float64 `json:"price"`
-	Product        Product `json:"product"`
+	CartId         int      `json:"cart_id" bson:"cart_id"`
+	StoreProductId int      `json:"store_product_id"`
+	ProductId      string   `json:"product_id"`
+	Quantity       int      `json:"quantity"`
+	Price          float64  `json:"price"`
+	Product        *Product `json:"product"`
 }
