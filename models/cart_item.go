d45104b413fe3ffbc5d8f247887ae6707613bbe3
Tomas Flores Alvear <tom@Tomass-MacBook-Pro-2.local>
2022-02-10 23:41:29 -0300

    fix: ignores product in cart item

--- models/cart_item.go
+++ models/cart_item.go
@@ -11,5 +11,5 @@ type CartItem struct {
 	ProductId      string   `json:"product_id"`
 	Quantity       int      `json:"quantity"`
 	Price          float64  `json:"price"`
-	Product        *Product `json:"product"`
+	Product        *Product `json:"product" gorm:"-"`
 }
