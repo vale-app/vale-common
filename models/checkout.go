d6edb5fe98203465cecd2d9e26d8cd8a655cfa53
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2022-03-01 01:07:02 -0300

    fix: adds internal id and corrects currency typegp

--- models/checkout.go
+++ models/checkout.go
@@ -1,14 +1,28 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	uuid "github.com/satori/go.uuid"
+	"github.com/vale-app/vale-common/enums"
+	"gorm.io/gorm"
+)
 
 type Checkout struct {
 	gorm.Model
-	CartId          int     `gorm:"column:cart_id" json:"cart_id"`
-	PaymentMethodId string  `gorm:"column:payment_method_id" json:"payment_method_id"`
-	Amount          float32 `gorm:"column:amount" json:"amount"`
-	Currency        string  `gorm:"column:currency" json:"currency"`
-	Discount        float32 `gorm:"column:discount" json:"discount"`
-	DiscountPercent float32 `gorm:"column:discount_percent" json:"discount_percent"`
-	StoreId         int     `gorm:"column:store_id" json:"store_id"`
+	CartId          int            `gorm:"column:cart_id" json:"cart_id"`
+	PaymentMethodId string         `gorm:"column:payment_method_id" json:"payment_method_id"`
+	Amount          float32        `gorm:"column:amount" json:"amount"`
+	Currency        enums.Currency `gorm:"column:currency" json:"currency"`
+	Discount        float32        `gorm:"column:discount" json:"discount"`
+	DiscountPercent float32        `gorm:"column:discount_percent" json:"discount_percent"`
+	StoreId         int            `gorm:"column:store_id" json:"store_id"`
+	InternalId      string         `json:"internal_id"`
+}
+
+// BeforeCreate will set a UUID rather than numeric ID.
+func (s *Checkout) BeforeCreate(tx *gorm.DB) (err error) {
+	newUUID := uuid.NewV4()
+
+	tx.Statement.SetColumn("InternalId", newUUID.String())
+
+	return
 }
