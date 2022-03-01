019dc00ce2be31152db37def6dbf7c5bb63735db
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2022-03-01 00:29:22 -0300

    feat: adds checkout model

--- models/checkout.go
+++ models/checkout.go
@@ -1,6 +1,14 @@
 package models
 
+import "gorm.io/gorm"
+
 type Checkout struct {
-	CartId          string `json:"cart_id"`
-	PaymentMethodId string `json:"payment_method_id"`
+	gorm.Model
+	CartId          string  `gorm:"column:cart_id" json:"cart_id"`
+	PaymentMethodId string  `gorm:"column:payment_method_id" json:"payment_method_id"`
+	Amount          float32 `gorm:"column:amount" json:"amount"`
+	Currency        string  `gorm:"column:currency" json:"currency"`
+	Discount        float32 `gorm:"column:discount" json:"discount"`
+	DiscountPercent float32 `gorm:"column:discount_percent" json:"discount_percent"`
+	StoreId         int     `gorm:"column:store_id" json:"store_id"`
 }
