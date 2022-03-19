fb76a470098fceaed6fe4f1b68759a11c59bfc86
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2022-03-19 14:12:12 -0300

    refactor!: removes gorm model for product

--- models/product.go
+++ models/product.go
@@ -1,7 +1,5 @@
 package models
 
-import "github.com/kamva/mgm/v3"
-
 type Image struct {
 	URL   string `json:"name:url" type:"float"`
 	Title string `json:"name:title" type:"string"`
@@ -14,30 +12,29 @@ type Measures struct {
 }
 
 type Product struct {
-	mgm.DefaultModel `bson:",inline"`
-	Name             string   `json:"name"`
-	Status           string   `json:"status" default:"ACTIVE"`
-	Category         string   `json:"category"`
-	SubCategory      string   `json:"sub_category"`
-	Description      string   `json:"description"`
-	ReferencePrice   float64  `json:"reference_price" bson:"reference_price"`
-	Discontinued     bool     `json:"discontinued" default:"false"`
-	InternalId       string   `json:"internal_id"`
-	IsPharma         bool     `json:"is_pharma" bson:"is_pharma" default:"false"`
-	IsAlcoholic      bool     `json:"is_alcoholic" bson:"is_alcoholic" default:"false"`
-	IsCigarette      bool     `json:"is_cigarette" bson:"is_cigarette" default:"false"`
-	NeedsRx          bool     `json:"needs_rx" bson:"needs_rx" default:"false"`
-	Maker            string   `json:"maker"`
-	TradeMark        string   `json:"trade_mark" bson:"trade_mark"`
-	Images           []Image  `json:"images"`
-	Measures         Measures `json:"measures"`
-	Metadata         string   `json:"metadata"`
-	Packing          string   `json:"packing"`
-	Weight           float64  `json:"weight"`
-	ContainedUnits   uint32   `json:"contained_units" bson:"contained_units"`
-	Ean              string   `json:"ean" bson:"ean"`
-	Sku              string   `json:"sku" bson:"sku"`
-	IsForAdult       bool     `json:"is_for_adult" bson:"is_for_adult" default:"false"`
+	Name           string   `json:"name"`
+	Status         string   `json:"status" default:"ACTIVE"`
+	Category       string   `json:"category"`
+	SubCategory    string   `json:"sub_category"`
+	Description    string   `json:"description"`
+	ReferencePrice float64  `json:"reference_price" bson:"reference_price"`
+	Discontinued   bool     `json:"discontinued" default:"false"`
+	InternalId     string   `json:"internal_id"`
+	IsPharma       bool     `json:"is_pharma" bson:"is_pharma" default:"false"`
+	IsAlcoholic    bool     `json:"is_alcoholic" bson:"is_alcoholic" default:"false"`
+	IsCigarette    bool     `json:"is_cigarette" bson:"is_cigarette" default:"false"`
+	NeedsRx        bool     `json:"needs_rx" bson:"needs_rx" default:"false"`
+	Maker          string   `json:"maker"`
+	TradeMark      string   `json:"trade_mark" bson:"trade_mark"`
+	Images         []Image  `json:"images"`
+	Measures       Measures `json:"measures"`
+	Metadata       string   `json:"metadata"`
+	Packing        string   `json:"packing"`
+	Weight         float64  `json:"weight"`
+	ContainedUnits uint32   `json:"contained_units" bson:"contained_units"`
+	Ean            string   `json:"ean" bson:"ean"`
+	Sku            string   `json:"sku" bson:"sku"`
+	IsForAdult     bool     `json:"is_for_adult" bson:"is_for_adult" default:"false"`
 }
 
 /*func (p *Product) Creating(ctx context.Context) error {
