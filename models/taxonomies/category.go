46397c163ab492a25fd70992d07246c849c9fd67
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2022-03-23 21:31:32 -0300

    fix: adds reference metadata

--- models/taxonomies/category.go
+++ models/taxonomies/category.go
@@ -13,7 +13,7 @@ type SubCategory struct {
 	Id         uint     `json:"id" gorm:"primaryKey"`
 	Name       string   `json:"name"`
 	CategoryId uint     `json:"category_id"`
-	Category   Category `json:"category"`
+	Category   Category `json:"category" gorm:"references:Id"`
 }
 
 type Segment struct {
@@ -21,7 +21,7 @@ type Segment struct {
 	Id            uint        `json:"id" gorm:"primaryKey"`
 	Name          string      `json:"name"`
 	SubCategoryId uint        `json:"sub_category_id"`
-	SubCategory   SubCategory `json:"sub_category"`
+	SubCategory   SubCategory `json:"sub_category" gorm:"references:Id"`
 }
 
 type SubSegment struct {
@@ -29,5 +29,5 @@ type SubSegment struct {
 	Id        uint    `json:"id" gorm:"primaryKey"`
 	Name      string  `json:"name"`
 	SegmentId uint    `json:"segment_id"`
-	Segment   Segment `json:"segment"`
+	Segment   Segment `json:"segment" gorm:"references:Id"`
 }
