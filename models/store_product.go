0cdabcbceba03483097b7f0ea6345d846853f70d
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2022-03-24 15:37:54 -0300

    revert: product reference back in store product

--- models/store_product.go
+++ models/store_product.go
@@ -7,19 +7,20 @@ import (
 
 type StoreProduct struct {
 	gorm.Model
-	StoreId        uint    `json:"store_id"`
-	Category       string  `json:"category"`
-	SubCategory    string  `json:"sub_category"`
-	ProductId      string  `json:"product_id"`
-	AvailableUnits int     `json:"available_units"`
-	ReservedUnits  int     `json:"reserved_units"`
-	Price          float32 `json:"price"`
-	Status         string  `json:"status" gorm:"default:'ACTIVE'"`
-	InternalId     string  `json:"internal_id"`
-	IsCustom       bool    `json:"is_custom" gorm:"default:false"`
-	Ean            string  `json:"ean"`
-	Sku            string  `json:"sku"`
-	ProductName    string  `json:"product_name"`
+	StoreId        uint     `json:"store_id"`
+	Category       string   `json:"category"`
+	SubCategory    string   `json:"sub_category"`
+	ProductId      string   `json:"product_id"`
+	AvailableUnits int      `json:"available_units"`
+	ReservedUnits  int      `json:"reserved_units"`
+	Price          float32  `json:"price"`
+	Status         string   `json:"status" gorm:"default:'ACTIVE'"`
+	InternalId     string   `json:"internal_id"`
+	IsCustom       bool     `json:"is_custom" gorm:"default:false"`
+	Ean            string   `json:"ean"`
+	Sku            string   `json:"sku"`
+	ProductName    string   `json:"product_name"`
+	Product        *Product `json:"product"`
 }
 
 // BeforeCreate will set a UUID rather than numeric ID.
